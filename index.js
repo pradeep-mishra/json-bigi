4972edef3cf4e4bfc9c987c4bff6b2fe44a20688
pradeep <pradeep23oct@gmail.com>
2020-09-26 03:11:20 +0530

    update

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 var stringify = JSON.stringify.bind(JSON);
 var parse = JSON.parse.bind(JSON);
-var isBigx = /:[\s\t\r]*([0-9]{15,})[\s\r\t]*[,|\}]/g;
-var isBig = /(?<=:[\s\t\r]*)([0-9]{15,})(?=[\s\r\t]*[,|\}])/g;
-var isBigNumber = /^([0-9]{15,})n$/g;
+var bigTracker = /(?<=[:\s\t\r\[\n,]+)([0-9]{17,})(?=[\s\r\t]*[,\}\]])/g;
+var isBigNumber = /^([0-9]{17,})n$/g;
+var strict = false;
+var bigSupport = typeof (BigInt);
 
 //JSON.parse = cParse;
 //JSON.stringify = cStringify;
 //JSON._parse = parse;
 //JSON._stringify = stringify;
 
-if (typeof (BigInt) == 'undefined') {
-    throw Error('BingInt is required, Please upgrade to nodejs 10.4 and above');
+
+function safer(bool) {
+    if (bool) {
+        bigTracker = /(?<=[:\s\t\r\[\n,]+)([0-9]{15,})(?=[\s\r\t]*[,\}\]])/g;
+        isBigNumber = /^([0-9]{15,})n$/g;
+    } else {
+        bigTracker = /(?<=[:\s\t\r\[\n,]+)([0-9]{17,})(?=[\s\r\t]*[,\}\]])/g;
+        isBigNumber = /^([0-9]{17,})n$/g;
+    }
 }
 
+
 function addBigInt(key, value) {
-    if (typeof (value) == "string" && value.match(isBigNumber)) {
+    if (typeof (value) == "string" && (value.match(isBigNumber) || value == "9999999999999999")) {
         //console.log('2', value)
-        return BigInt(value.slice(0, -1));
+        if (bigSupport != 'undefined') {
+            return BigInt(value.slice(0, -1));
+        }
+        return value.slice(0, -1);
     }
     return value;
 }
 
 function cParse(json) {
-    if (json.match(isBig)) {
-        let jtxt = json.replace(isBig, '"$1n"');
+    if (json.match(bigTracker)) {
+        let jtxt = json.replace(bigTracker, '"$1n"');
         //console.log('1', jtxt)
         return parse(jtxt, addBigInt);
     }
@@ -47,5 +59,6 @@ function cStringify(obj) {
 
 module.exports = {
     parse: cParse,
-    stringify: cStringify
+    stringify: cStringify,
+    safer: safer
 }
